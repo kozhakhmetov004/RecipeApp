5a14d17ee26539233389797c4da1fbd882acf8ac
kozhakhmetov004 <164349471+kozhakhmetov004@users.noreply.github.com>
2024-10-03 18:14:08 +0500

    first upload

---
+++ app/src/main/java/mykotlin/nu/myrecipeappcourse/RecipeScreen.kt
@@ -0,0 +1,88 @@
+package mykotlin.nu.myrecipeappcourse
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ProgressIndicatorDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.rememberAsyncImagePainter
+
+@Composable
+fun RecipeScreen(modifier: Modifier=Modifier,navigateToDetail:(Category)->Unit, viewState:MainViewModel.RecipeState){
+
+
+
+    Box(modifier = Modifier.fillMaxSize()){
+        when{
+            viewState.loading->{
+                CircularProgressIndicator(progress = 0.89f, modifier.align(Alignment.Center))
+            }
+            viewState.error!=null->{
+                Text(text = "ERROR OCCURRED!")
+            }
+            viewState.list.isNotEmpty() -> {
+                // Display categories
+                CategoryScreen(categories = viewState.list, navigateToDetail)
+            }
+            else -> {
+                Text(text = "No Categories Found")
+            }
+        }
+    }
+}
+
+
+@Composable
+fun CategoryScreen(categories: List<Category>,navigateToDetail:(Category)->Unit){
+    LazyVerticalGrid(GridCells.Fixed(2), modifier = Modifier.fillMaxSize() ){
+        items(categories){
+            category->
+            CategoryItem(category = category,navigateToDetail=navigateToDetail)
+        }
+    }
+}
+
+@Composable
+fun CategoryItem(category: Category, navigateToDetail:(Category)->Unit){
+    Column(modifier = Modifier
+        .padding(8.dp)
+        .fillMaxSize().
+        clickable {
+                  navigateToDetail(category)
+        }, horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(painter = rememberAsyncImagePainter(category.strCategoryThumb), contentDescription = null,
+            modifier = Modifier
+                .fillMaxSize()
+                .aspectRatio(1f)
+            )
+
+        Text(text = category.strCategory,
+            color = Color.Black,
+            style = TextStyle(fontWeight = FontWeight.Bold),
+            modifier = Modifier.padding(top=4.dp)
+            )
+
+    }
+}
---
+++ app/src/main/java/mykotlin/nu/myrecipeappcourse/Screen.kt
@@ -0,0 +1,6 @@
+package mykotlin.nu.myrecipeappcourse
+
+sealed class Screen(val route: String) {
+    object RecipeScreen:Screen("recipescreen")
+    object DetailScreen:Screen("detailscreen")
+}
